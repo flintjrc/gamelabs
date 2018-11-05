fffb182784870fff8c7c217aa3cb7eb703d2b299
Jean-Raphaël Cornel <jraph.cornel@gmail.com>
2018-11-05 19:48:10 +0100

    gym11 done

---
+++ GymTp2/Assets/Script/Gym22_torchLife.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gym22_torchLife : MonoBehaviour {
+
+	[SerializeField] float torchMaxLifetime = 30f;
+	[SerializeField] float torchFuel;
+	[SerializeField] float burnSpeed = 1.0f;
+	[SerializeField] public UnityEngine.UI.Slider fuelBar;
+	bool torchIsLit = true;
+	SpriteRenderer[] lChildRenderers;
+
+	void Start(){
+		lChildRenderers=gameObject.GetComponentsInChildren<SpriteRenderer>();
+		fuelBar.value = ((torchFuel/torchMaxLifetime) * 100);
+	}
+
+	void lightTorch(bool instruction){
+        foreach ( SpriteRenderer lRenderer in lChildRenderers) {
+			if(lRenderer.gameObject.name =="Torch"){ lRenderer.enabled= instruction;}
+		}
+	}
+
+	void Update(){
+
+		if(Input.GetAxis("Mouse ScrollWheel")!=0 && torchFuel>0){
+			torchIsLit = !torchIsLit;
+			lightTorch(torchIsLit);
+		}
+
+		if(torchIsLit){
+			torchFuel = Mathf.Min(torchFuel, torchMaxLifetime);
+			torchFuel -= burnSpeed*Time.deltaTime;
+			fuelBar.value = ((torchFuel/torchMaxLifetime) * 100);
+		}
+		if(torchFuel<=0){
+			lightTorch(false);
+			torchIsLit = false;
+		}
+	}
+
+	void addFuel(float fuelAdded){
+		torchFuel=Mathf.Max(torchFuel+fuelAdded, torchMaxLifetime);
+		fuelBar.value = ((torchFuel/torchMaxLifetime) * 100);
+	}
+}
