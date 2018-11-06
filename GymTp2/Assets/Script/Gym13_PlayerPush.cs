48b907a135088891204d4d7ed0f68ad1f3094492
Stephanie Leclerc <stephanie.leclerc@polymtl.ca>
2018-11-05 21:34:22 -0500

    Mecanique push and pull fonctionelle

---
+++ GymTp2/Assets/Script/Gym13_PlayerPush.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gym13_PlayerPush : MonoBehaviour {
+
+    public float distance = 1f;
+    public LayerMask boxMask;
+    public KeyCode grabInput;
+    public KeyCode releaseInput;
+
+    GameObject box;
+    bool isAttached = false;
+    Collider attachedCollider = null;
+
+
+    private void Update()
+    {
+        if (isAttached && Input.GetKeyDown(releaseInput))
+        {            
+            attachedCollider.GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(attachedCollider.GetComponent<FixedJoint>());
+            attachedCollider = null;
+            isAttached = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!isAttached && collision.collider.gameObject.tag == "cube" && Input.GetKey(grabInput))
+        {
+            attachedCollider = collision.collider;
+            FixedJoint joint = collision.collider.gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = GetComponent<Rigidbody>();
+            isAttached = true;
+        }
+    }
+    
+}
