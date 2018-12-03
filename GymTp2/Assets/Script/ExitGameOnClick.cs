fc3a3a037fa1b2eaf2d5129cdabe50e38f2ce7d0
vBouchard <valerie-3.bouchard@polymtl.ca>
2018-12-02 22:04:54 -0500

    main menu

---
+++ GymTp2/Assets/Script/ExitGameOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameOnClick : MonoBehaviour
+{
+    public void quitGame()
+    {
+        //allows to close the game (play mode) in the editor (found in unity tutorials)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        //allow to quit the game completely
+        Application.Quit();
+#endif
+    }
+}
---
+++ GymTp2/Assets/Script/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        //Load the scene given in parameters.
+        //IMPORTANT! Scenes must be added in the build settings first
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
