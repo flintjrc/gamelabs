0bbf4be2a694098b861e6bad232ba21ae7b22c5c
vBouchard <valerie-3.bouchard@polymtl.ca>
2018-12-03 17:05:36 -0500

    menu de pause in game

---
+++ GymTp2/Assets/Script/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    // Allows to verify if the game is paused
+    public static bool IsGamePaused;
+
+    //Represents the menu
+    public GameObject PauseMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.P))
+	    {
+	        if (IsGamePaused)
+	        {
+	            ResumeGame();
+	        }
+	        else
+	        {
+	            PauseGame();
+	        }
+	    }
+	}
+
+    void PauseGame()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+}
